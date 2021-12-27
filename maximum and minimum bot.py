13e7ac30cd9ea0b8b6b31ee74dc94d211b313921
Mayura0411 <96528334+Mayura0411@users.noreply.github.com>
2021-12-27 17:44:19 +0530

    Add files via upload

---
+++ maximum and minimum bot.py
@@ -0,0 +1,11 @@
+print("I am a maximum and minimum bot")
+a=int(input("first number:"))
+b=int(input("second number:"))
+if a>b:
+    print(a," is the maximum number")
+    print(b,"is the minimun number")
+elif a<b:
+    print(b,"is the maximum number")
+    print(a,"is the minimum number")
+else:
+    print(a,"and",b,"are equal")
